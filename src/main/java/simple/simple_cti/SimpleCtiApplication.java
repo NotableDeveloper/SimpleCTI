6deaaf3ff86dc4b1adaa81ee91d96d82ee903a68
seongjin <castlereal01@gmail.com>
2025-12-06 12:59:16 +0900

    Gemini.md 추적 제외

--- src/main/java/simple/simple_cti/SimpleCtiApplication.java
+++ src/main/java/simple/simple_cti/SimpleCtiApplication.java
@@ -3,10 +3,10 @@ package simple.simple_cti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SimpleCtiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleCtiApplication.class, args);
     }
+
 }
