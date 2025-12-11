0d5e318f5120cb6200fb952398e0e5f98f1d3873
seongjin <castlereal01@gmail.com>
2025-12-11 23:39:58 +0900

    Dial 전담 컨트롤러 작성

---
+++ src/main/java/simple/simple_cti/controller/DialController.java
@@ -0,0 +1,38 @@
+package simple.simple_cti.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import simple.simple_cti.ami.OriginateCommandExecutor;
+import org.asteriskjava.manager.response.ManagerResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DialController {
+
+    private final OriginateCommandExecutor originateCommandExecutor;
+
+    public DialController(OriginateCommandExecutor originateCommandExecutor) {
+        this.originateCommandExecutor = originateCommandExecutor;
+    }
+
+    @PostMapping("/originate")
+    @ResponseBody
+    public Map<String, Object> originate(@RequestParam String targetNumber) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            ManagerResponse managerResponse = originateCommandExecutor.executeOriginate(targetNumber);
+
+            response.put("success", true);
+            response.put("message", "Call initiated successfully!");
+            response.put("amiResponse", managerResponse.getResponse());
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("message", "Failed to initiate call: " + e.getMessage());
+        }
+        return response;
+    }
+}
