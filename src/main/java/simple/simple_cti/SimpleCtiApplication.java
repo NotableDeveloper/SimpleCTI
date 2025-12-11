47ad4c32092f025622e3d76b92b1ba6e32b92449
seongjin <castlereal01@gmail.com>
2025-12-11 23:40:42 +0900

    홈 화면 수정, Dial 화면 구현

--- src/main/java/simple/simple_cti/SimpleCtiApplication.java
+++ src/main/java/simple/simple_cti/SimpleCtiApplication.java
@@ -8,5 +8,4 @@ public class SimpleCtiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimpleCtiApplication.class, args);
     }
-
 }
--- src/main/java/simple/simple_cti/controller/HomeController.java
+++ src/main/java/simple/simple_cti/controller/HomeController.java
@@ -2,11 +2,23 @@ package simple.simple_cti.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; // Added import
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/dial") // Changed from /originate
+    public String dialPage() { // Changed method name for clarity
+        return "dial"; // Returns dial.html
+    }
+
+    @PostMapping("/navigateToDial") // New method to handle navigation from home
+    public String navigateToDial() {
+        return "redirect:/dial"; // Redirects to the /dial GET endpoint
     }
 }
