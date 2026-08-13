518167120ba3e4a850266596f28dfb843fe29b0a
castlereal01 <castlereal01@gmail.com>
2026-08-13 20:56:33 +0900

    refactor: remove ARS feature and related classes

--- src/main/java/simple/simple_cti/repository/ArsScenarioRepository.java
+++
@@ -1,7 +0,0 @@
-package simple.simple_cti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import simple.simple_cti.domain.ArsScenario;
-
-public interface ArsScenarioRepository extends JpaRepository<ArsScenario, Long> {
-}
